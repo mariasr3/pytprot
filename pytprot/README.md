4dd96bb6cb946d42e72463b86197f9c3342fdfb4
Othmane Hayoun Mya <74412118+othmya@users.noreply.github.com>
2021-03-16 18:41:38 +0100

    Delete pytprot directory

--- pytprot/README.md
+++
@@ -1,9 +0,0 @@
-# pyprot
-
-**pytprot** is a web-app and command-line package to build protein multicomplexes from
-binary chain interactions.
-
-# 1. Installation
-
-Either 
-
--- pytprot/pytprot/__init__.py
+++
@@ -1,7 +0,0 @@
-
-
-## If we want to import all modules in the package
-__all__ = ["argparse", "dash_test", "functions", "main", "test_pyprot_dash"] # For now, we just have one
-
-
-#from I_O_args import *
