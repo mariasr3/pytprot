6684f013a61db6cb99b3866382e7cf5832847eda
Othmane Hayoun Mya <74412118+othmya@users.noreply.github.com>
2021-03-16 19:26:10 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -12,10 +12,17 @@ setup(name = "pytprot",
       long_description=open('README.md').read(),
       author="Maria Sopena, Joana Llauradó, Othmane Hayoun",
       license='LICENSE.txt',
+      url='https://github.com/mariasr3/pytprot',
       packages=setuptools.find_packages(),
       scripts=['./pytprot/main.py', './pytprot/__init__.py', './pytprot/parser.py', './pytprot/functions.py',
                './pytprot/test_pyprot_dash.py'],
-      install_requires=["BioPython", "numpy", "seaborn", "matplotlib", "setuptools"])
+      install_requires=["BioPython", "numpy", "seaborn", "matplotlib", "setuptools", "dash", "time"],
+      classifiers=[
+            'Development Status :: 0.5 - Beta',
+            'Intended Audience :: Bioinformaticians',  # Define that your audience are developers
+            'Topic :: Software Development :: Bioinformatic Tools',
+            'License :: OSI Approved :: MIT License',  # Again, pick a license
+            'Programming Language :: Python :: 3.7'])
 
 
 
