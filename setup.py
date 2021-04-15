fddb89aac53d5ad814ea2fd5f19eb002b680bd98
mariasr3 <74106043+mariasr3@users.noreply.github.com>
2021-04-15 20:31:37 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 
+#!/usr/bin/env python
+
+
 # pytprot SETUP FILE
 ##############################
 
@@ -7,7 +10,7 @@ from distutils.core import setup, Extension
 import setuptools
 
 setup(name = "pytprot",
-      version='0.5',
+      version='1.0.0',
       description="Macrocomplex builder from PPIs",
       long_description_content_type='text/markdown',
       long_description=open('README.md').read(),
@@ -15,9 +18,9 @@ setup(name = "pytprot",
       license='LICENSE.txt',
       url='https://github.com/mariasr3/pytprot',
       packages=setuptools.find_packages(),
-      scripts=['./pytprot/main.py', './pytprot/__init__.py', './pytprot/parser.py', './pytprot/functions.py',
-               './pytprot/test_pyprot_dash.py'],
-      install_requires=["BioPython", "numpy", "seaborn", "matplotlib", "setuptools", "dash", "time"])
+      scripts=['./pytprot/pytprot.py', './pytprot/__init__.py', './pytprot/parser.py', './pytprot/chainfunctions.py',
+               './pytprot/pytprot_dash.py', './pytprot/inputfunctions.py', './pytprot/modelfunctions.py'],
+      install_requires=["BioPython", "setuptools", "dash"])
 
 
 
